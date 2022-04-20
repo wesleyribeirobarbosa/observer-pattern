2c6cb263705bbf49389804d210e564201a89df90
wesleyribeirobarbosa <wesleysrs_95@hotmail.com>
2022-04-19 23:28:55 -0300

    1:n description

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ new Observer1(subject);
 new Observer2(subject);
 new Observer3(subject);
 
+// Updating only the Subject, all the observers will be notified (1:n relation)
 setInterval(function() {
   subject.setState(Math.floor(Math.random() * 10));
   console.log('\n');
